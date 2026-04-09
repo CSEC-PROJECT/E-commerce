b4dc1618962d653487c0d36a7b1ffcb386bfd7e6
fromsagirma <firomsagirma@astu.edu.et@gmail.com>
2026-04-09 01:56:33 -0700

    feat: initialize App component with NavBar integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 // import ProductsPage from './pages/ProductsPage'
-import DetailsPage from './pages/DetailsPage'
+// import DetailsPage from './pages/DetailsPage'
+import NavBar from './components/Common/NavBar'
+// import Card1 from './components/Common/Card1'
+// import Card2 from './components/Common/Card2'
+// import Card4 from './components/Common/Card4'
+// import Card3 from './components/Common/Card3'
+// import Footer from './components/Common/Footer'
 const App = () => {
   return (
     <>
-    {/* <ProductsPage/> */}
-    <DetailsPage/>
+      {/* <ProductsPage/> */}
+      {/* <DetailsPage/> */}
+      <NavBar />
+      {/* <Card1 /> */}
+      {/* <Card2 /> */}
+      {/* <Card4 /> */}
+      {/* <Card3 /> */}
+      {/* <Footer /> */}
     </>
   )
 }
