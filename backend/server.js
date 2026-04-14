166e3de434e7f1639bb897fdc62e4b22e0ae0174
Fozia31 <foziyadamtew00@gmail.com>
2026-04-14 23:04:30 +0400

    docs: fix swagger path and add definition for deployment

--- backend/server.js
+++ backend/server.js
@@ -14,8 +14,13 @@ import cartUserRoutes from "./routes/user/cart.route.js";
 
 import swaggerUi from "swagger-ui-express";
 import YAML from "yamljs";
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const swaggerDocument = YAML.load("./docs/swagger.yaml");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const swaggerDocument = YAML.load(path.join(__dirname, "docs", "swagger.yaml"));
 
 
 dotenv.config();
